00ab797eaafc8016a9afb31ceed5593c9733e567
Jugger Chin <59953566+JuggerGuGu@users.noreply.github.com>
2026-07-02 21:37:16 +0900

    学习2

--- Assets/Xuexi/Xuexi.cs
+++ Assets/Xuexi/Xuexi.cs
@@ -7,7 +7,7 @@ public class Xuexi : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Xuexi start");
     }
 
     // Update is called once per frame
