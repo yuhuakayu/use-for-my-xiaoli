80db28263338fcf4e3ca4c8ca555339c580c38bf
Jugger Chin <59953566+JuggerGuGu@users.noreply.github.com>
2026-07-02 21:57:31 +0900

    未接教学

--- Assets/weijie/weijie.cs
+++ Assets/weijie/weijie.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("阿飞");
     }
 
     // Update is called once per frame
